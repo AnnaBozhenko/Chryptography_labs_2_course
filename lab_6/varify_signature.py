61bef721d192c9d9ae0a8678f85b6b52a43cbe26
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2022-06-10 00:20:10 +0300

    Added decrypt function (rsa algorythm).

--- lab_6/varify_signature.py
+++ lab_6/varify_signature.py
@@ -3,30 +3,40 @@ import pprint
 import re
 from hash_functions import encode_content_sha1
 from display_keys import get_key, log_msg
+from hash_sign import SHA1_BIT_LENGTH
 
 BLOCK_SIZE = 2**20
 KEY_SIZE = 1024
 FILE_NAME = 'zayava.docx'
-PRIVATE_KEY_FILE = f'private_key_{KEY_SIZE}.pem'
-# PUBLIC_KEY_FILE = 'public_key_1024.pem'
+PUBLIC_KEY_FILE = 'public_key_1024.pem'
+SHA1_BIT_LENGTH = 20 * 8
 raw_file = open(FILE_NAME, 'rb').read()
 
+
+def decrypt_rsa(cipher, public_key, l: int):
+    k = public_key.e
+    n = public_key.n
+    cipher_int = int.from_bytes(cipher, byteorder = "big")
+    message_int = pow(cipher_int, k, n)
+    bi_message = message_int.to_bytes(l, byteorder = "big")
+    return bi_message
+
 # get hash of the file
-_hash = encode_content_sha1(FILE_NAME, BLOCK_SIZE).hexdigest()
-log_msg(f"Hash from file: {_hash}")
+hashed_data = encode_content_sha1(FILE_NAME, BLOCK_SIZE).digest()
 
 # get content of signature
 with open(FILE_NAME + '.sgn', 'rb') as inf:
     signature = inf.read()
 
 # get content of publ key 
-priv_key = get_key(PRIVATE_KEY_FILE, 'pr')
-# publ_key = get_key(PUBLIC_KEY_FILE, 'pb')
+publ_key = get_key(PUBLIC_KEY_FILE, 'pb')
 
 # get hash and metadata with the signature and public key
-message = rsa.decrypt(signature, priv_key)
-log_msg(f"Extracted metadata:")
-pprint.pprint(message)
-_hash_from_signature = re.findall('file hash: (.+)', message.decode('utf-8'))[0]
-log_msg(f"Hash from signature: {_hash_from_signature}")
-log_msg(f"Varification was successful: {_hash == _hash_from_signature}")
+metadata_and_hash = decrypt_rsa(signature, publ_key, SHA1_BIT_LENGTH)
+log_msg(f"Extracted metadata: \n{metadata_and_hash[:-20].decode(encoding='utf8')}{metadata_and_hash[-20:]}")
+
+
+hash_from_signature = metadata_and_hash[-20:]
+log_msg(f"Hash from file: {hashed_data}")
+log_msg(f"Hash from signature: {hash_from_signature}")
+log_msg(f"Varification was successful: {hashed_data == hash_from_signature}")
